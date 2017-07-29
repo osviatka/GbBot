dd395415fb0a9cc51356b45af04cea4b7d46fb2c
osviatka <svyatka@ukr.net>
2017-07-29 21:56:31 +0300

    add func listen messages

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api'),
+    token = '356188286:AAErLrRuXStooS6VVUCM1CVgMNYygb837vY',
+    bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', function(msg) {
+    var chatId = msg.chat.id;
+
+// send a message to the chat acknowledging receipt of their message
+bot.sendMessage(chatId, 'Received your message');
+});
