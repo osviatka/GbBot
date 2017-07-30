3defc34b55cfeedbcf49b54dc66c5520d1e2223f
osviatka <svyatka@ukr.net>
2017-07-30 15:02:15 +0300

    add music file&run

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var TelegramBot = require('node-telegram-bot-api'),
+    fs = require('fs'),
     token = '356188286:AAErLrRuXStooS6VVUCM1CVgMNYygb837vY',
     bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', function(msg) {
-    var chatId = msg.chat.id;
-
-// send a message to the chat acknowledging receipt of their message
-bot.sendMessage(chatId, 'Received your message');
+    var chatId = msg.chat.id,
+    file = __dirname+'/town.mp3';
+bot.sendAudio(chatId, file);
 });
