4badd0c51d5aea38ebd27e42b9f5f0d8e3f78545
osviatka <svyatka@ukr.net>
2017-07-31 14:56:41 +0300

    upgrade mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,20 +1,49 @@
 var TelegramBot = require('node-telegram-bot-api'),
-
+    dbConfig = 'mongodb://admin:root@ds129733.mlab.com:29733/gbbot'
     token = '356188286:AAErLrRuXStooS6VVUCM1CVgMNYygb837vY',
     bot = new TelegramBot(token, {polling: true});
 
 var login = null;
 var password = null;
+var field = null;
 
 bot.on('message', function (msg) {
 
-    if (msg.text == '/enter' || login != null){
-        login ();
+    if (msg.text == '/enter'){
+        field = 'login';
+        login (chatId);
+    }
+    else if  (login != null){
+        login (chatId, msg.text);
     }
     else{
         defaultCommand(chatId);
     }
 })
+
+function login(chatId, text) {
+    if (login == 'login'){
+        if (text){
+            login = text;
+            field = 'password';
+        }
+        else{
+            bot.sendMessage('Enter your login');
+        }
+    }
+    if (field == 'password'){
+        if (text){
+            password = text;
+            checkAccess(chatId);
+        }
+        else{
+            bot.sendMessage('Enter your password');
+        }
+    }
+}
+function checkAccess(chatId) {
+    bot.sendMessage('Blocked');
+}
 
 function  defaultCommand(chatId) {
     bot.sendMessage('Hello! It is defult command :)');
