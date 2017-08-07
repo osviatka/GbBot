2f557a239a3de94d487a8369a74a8c2fbec37d09
osviatka <svyatka@ukr.net>
2017-08-07 20:32:12 +0300

    index

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var TelegramBot = require('node-telegram-bot-api'),
     token = '356188286:AAErLrRuXStooS6VVUCM1CVgMNYygb837vY',
     bot = new TelegramBot(token, {polling: true});
 
+
 bot.on('message', function(msg) {
     var chatId = msg.chat.id,
         file = __dirname+'/town.mp3';
     bot.sendAudio(chatId, file);
 });
+
